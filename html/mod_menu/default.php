e93e7477b728f822076fe8093b67f6c46de1e4de
Niels Nübel <n.nuebel@nn-medienagentur.de>
2014-01-23 10:49:36 +0100

    add \n for code style

--- html/mod_menu/default.php
+++ html/mod_menu/default.php
@@ -10,108 +10,97 @@
 defined('_JEXEC') or die;
 // Note. It is important to remove spaces between elements.
 $last_level_one_id = 0;
-foreach ($list as $menu){
-    if($menu->level == 1) {
+foreach ($list as $menu) {
+    if ($menu->level == 1) {
         $last_level_one_id = $menu->id;
         break;
     }
 }
 $first_start = true;
-?>
-<?php // The menu class is deprecated. Use nav instead. ?>
-<ul class="nav menu<?php echo $class_sfx;?>"<?php
-	$tag = '';
-	if ($params->get('tag_id') != null)
-	{
-		$tag = $params->get('tag_id').'';
-		echo ' id="'.$tag.'"';
-	}
-?>>
-<?php
-foreach ($list as $i => &$item) :
-	$class = 'item-'.$item->id.' li'.$i;
-	if ($item->id == $active_id)
-	{
-		$class .= ' current';
-	}
+$tag = null;
+$ulid = null;
+if ($params->get('tag_id') != null) {
+    $tag = $params->get('tag_id');
+    $ulid = 'id="'.$tag.'"';
+}
+echo '<ul class="nav menu'.$class_sfx.'" '.$ulid.'>'."\n";
+    foreach ($list as $i => &$item) :
+        //Codestyle
+        $spacer = '    ';
+        for($i=1;$i < $item->level;$i++){
+            $spacer .= '    ';
+        }
+        if($item->level >1)$spacer .='    ';
 
-	if (in_array($item->id, $path))
-	{
-		$class .= ' active';
-	}
-	elseif ($item->type == 'alias')
-	{
-		$aliasToId = $item->params->get('aliasoptions');
-		if (count($path) > 0 && $aliasToId == $path[count($path) - 1])
-		{
-			$class .= ' active';
-		}
-		elseif (in_array($aliasToId, $path))
-		{
-			$class .= ' alias-parent-active';
-		}
-	}
+        $class = 'item-' . $item->id . ' li' . $i;
+        if ($item->id == $active_id) {
+            $class .= ' current';
+        }
 
-	if ($item->type == 'separator')
-	{
-		$class .= ' divider';
-	}
+        if (in_array($item->id, $path)) {
+            $class .= ' active';
+        } elseif ($item->type == 'alias') {
+            $aliasToId = $item->params->get('aliasoptions');
+            if (count($path) > 0 && $aliasToId == $path[count($path) - 1]) {
+                $class .= ' active';
+            } elseif (in_array($aliasToId, $path)) {
+                $class .= ' alias-parent-active';
+            }
+        }
 
-	///start first last changes
-	if($first_start){
-		$class .= ' first';
-		$first_start = false;
-	}
-	if ($item->shallower || $item == end($list) || $item->id == $last_level_one_id ) {
-		$class .= ' last';
-	}
-	
-	if ($item->deeper)
-	{
-		$class .= ' deeper';
-		$first_start = true;
-	}
+        if ($item->type == 'separator') {
+            $class .= ' divider';
+        }
 
-	if ($item->parent)
-	{
-		$class .= ' parent';
-	}
+        ///start first last changes
+        if ($first_start) {
+            $class .= ' first';
+            $first_start = false;
+        }
+        if ($item->shallower || $item == end($list) || $item->id == $last_level_one_id) {
+            $class .= ' last';
+        }
 
-	if (!empty($class))
-	{
-		$class = ' class="'.trim($class) .'"';
-	}
+        if ($item->deeper) {
+            $class .= ' deeper';
+            $first_start = true;
 
-	echo '<li'.$class.'>';
+        }
 
-	// Render the menu item.
-	switch ($item->type) :
-		case 'separator':
-		case 'url':
-		case 'component':
-		case 'heading':
-			require JModuleHelper::getLayoutPath('mod_menu', 'default_'.$item->type);
-			break;
+        if ($item->parent) {
+            $class .= ' parent';
+        }
 
-		default:
-			require JModuleHelper::getLayoutPath('mod_menu', 'default_url');
-			break;
-	endswitch;
+        if (!empty($class)) {
+            $class = ' class="' . trim($class) . '"';
+        }
 
-	// The next item is deeper.
-	if ($item->deeper)
-	{
-		echo '<ul class="nav-child unstyled small">';
-	}
-	// The next item is shallower.
-	elseif ($item->shallower)
-	{
-		echo '</li>';
-		echo str_repeat('</ul></li>', $item->level_diff);
-	}
-	// The next item is on the same level.
-	else {
-		echo '</li>';
-	}
-endforeach;
-?></ul>
+        echo $spacer.'<li' . $class. '>';
+        // Render the menu item.
+        switch ($item->type) :
+            case 'separator':
+            case 'url':
+            case 'component':
+            case 'heading':
+                require JModuleHelper::getLayoutPath('mod_menu', 'default_' . $item->type);
+                break;
+
+            default:
+                require JModuleHelper::getLayoutPath('mod_menu', 'default_url');
+                break;
+        endswitch;
+
+        // The next item is deeper.
+        if ($item->deeper) {
+            echo "\n".$spacer.'    <ul class="nav-child unstyled level' . $item->level . ' small">'."\n";
+        } // The next item is shallower.
+        elseif ($item->shallower) {
+            echo '</li>'."\n";
+            echo str_repeat(substr($spacer,0,-4).'</ul><!-- ul.level'.($item->level-1).' -->'."\n".substr($spacer,0,-8).'</li><!-- li.deeper -->'."\n", $item->level_diff);
+        } // The next item is on the same level.
+        else {
+            echo '</li>'."\n";
+        }
+    endforeach;
+    ?>
+</ul><!-- end .nav.menu -->
